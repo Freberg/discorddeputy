157aa94919d72a8e84a7844d3b9b045d7cd77915
Daniel Freberg <daniel.freberg@scila.se>
2020-10-30 09:42:15 +0100

    Move shared_net network definition from rabbit compose file to startup script

--- discorddeputy-deploy/scripts/compose_all_images.sh
+++ discorddeputy-deploy/scripts/compose_all_images.sh
@@ -1,3 +1,4 @@
+(docker network create --driver bridge shared_net || true) &&
 (cd discorddeputy-deploy/config/container/rabbitMQ || exit && docker-compose down && docker-compose up -d) &&
 (cd discorddeputy-deploy/config/container/mongoDB || exit && docker-compose down && docker-compose up -d) &&
 (cd discorddeputy-deploy/config/container/discorddeputy/bot || exit && docker-compose down && docker-compose up -d) &&
--- discorddeputy-deploy/scripts/compose_down_all_images.sh
+++ discorddeputy-deploy/scripts/compose_down_all_images.sh
@@ -4,4 +4,5 @@
 (cd discorddeputy-deploy/config/container/discorddeputy/api-steam || exit && docker-compose down) &&
 (cd discorddeputy-deploy/config/container/discorddeputy/bot || exit && docker-compose down) &&
 (cd discorddeputy-deploy/config/container/mongoDB || exit && docker-compose down) &&
-(cd discorddeputy-deploy/config/container/rabbitMQ || exit && docker-compose down)
+(cd discorddeputy-deploy/config/container/rabbitMQ || exit && docker-compose down) &&
+(docker network rm shared_net || true)
