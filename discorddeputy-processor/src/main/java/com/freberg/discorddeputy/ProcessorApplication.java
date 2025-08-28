7a6e25effd22fd5e0dfaf0586a7a244355429cad
Freberg <11931384+Freberg@users.noreply.github.com>
2025-08-28 14:38:43 +0200

    Fix: Graceful handling of duplicate notifications in processor service (#1)
    * handle duplicate notifications in processor service gracefully

--- discorddeputy-processor/src/main/java/com/freberg/discorddeputy/ProcessorApplication.java
+++ discorddeputy-processor/src/main/java/com/freberg/discorddeputy/ProcessorApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DuplicateKeyException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -19,15 +20,19 @@ public class ProcessorApplication {
 
     @Bean
     public Function<Flux<DiscordNotification>, Flux<DiscordNotification>> processor() {
-        return notifications -> notifications
-                .filterWhen(notification -> repository.existsById(notification.getId()).map(b -> !b))
-                .flatMap(this::persist);
+        return notifications -> notifications.flatMap(this::persist);
     }
 
     private Mono<DiscordNotification> persist(DiscordNotification discordNotification) {
-        log.info("Persisted new notification with ID \"{}\" to DB from source \"{}\"", discordNotification.getId(),
-                discordNotification.getSource());
-        return repository.save(discordNotification);
+        return repository.insert(discordNotification)
+                .doOnNext(savedNotification -> log.info("Persisted new notification with ID \"{}\" to DB from source \"{}\"",
+                        savedNotification.getId(), savedNotification.getSource()))
+                .onErrorResume(DuplicateKeyException.class, e -> {
+                    log.info("Notification with ID \"{}\" already exists in DB. Skipping.", discordNotification.getId());
+                    return Mono.empty();
+                })
+                .doOnError(e -> log.error("Error persisting notification with ID \"{}\" from source \"{}\"",
+                        discordNotification.getId(), discordNotification.getSource(), e));
     }
 
     public static void main(String[] args) {
