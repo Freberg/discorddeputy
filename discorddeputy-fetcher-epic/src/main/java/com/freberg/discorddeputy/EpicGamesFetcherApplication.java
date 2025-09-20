6e3ddb470a0f1a27a4979c2d661a4f1e767e1548
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2025-09-20 13:14:05 +0200

    removed transitive dependency commons-logging

--- discorddeputy-fetcher-epic/src/main/java/com/freberg/discorddeputy/EpicGamesFetcherApplication.java
+++ discorddeputy-fetcher-epic/src/main/java/com/freberg/discorddeputy/EpicGamesFetcherApplication.java
@@ -3,12 +3,13 @@ package com.freberg.discorddeputy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Flux;
 
 import java.util.function.Supplier;
 
-@SuppressWarnings("unused")
+@SpringBootApplication
 public class EpicGamesFetcherApplication {
 
     private static final Logger log = LoggerFactory.getLogger(EpicGamesFetcherApplication.class);
