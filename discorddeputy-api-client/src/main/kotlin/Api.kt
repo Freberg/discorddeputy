26019bad5570f285c9accbfcda2851ed7e81801f
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2025-09-28 15:37:25 +0200

    change http client used in api-client library, CIO uses sl4j as opposed to apache

--- discorddeputy-api-client/src/main/kotlin/Api.kt
+++ discorddeputy-api-client/src/main/kotlin/Api.kt
@@ -10,7 +10,7 @@ import com.google.gson.JsonSerializer
 import io.ktor.client.*
 import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.apache.*
+import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.serialization.gson.*
@@ -28,7 +28,7 @@ class ApiClient(val baseUrl: String, engine: HttpClientEngine) {
         }
     }
 
-    constructor(baseUrl: String) : this(baseUrl, Apache.create())
+    constructor(baseUrl: String) : this(baseUrl, CIO.create())
 
     suspend fun upcomingOffers(): List<DiscordNotification> {
         return client.get("$baseUrl/upcomingOffers").body()
