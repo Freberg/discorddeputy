dcbbf9638486adb6bf3dfbb0b2dd7269c9722979
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2026-01-06 16:23:23 +0100

    chore(deps): fixed compliation error by replacing javax annotation with jspecify

--- discorddeputy-fetcher-epic/src/main/java/com/freberg/discorddeputy/DiscordNotification.java
+++ discorddeputy-fetcher-epic/src/main/java/com/freberg/discorddeputy/DiscordNotification.java
@@ -3,8 +3,8 @@ package com.freberg.discorddeputy;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
+import org.jspecify.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.time.Instant;
 
 
