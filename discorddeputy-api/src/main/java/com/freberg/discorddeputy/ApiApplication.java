372c54dff8e54e94a2c0be4591d0e9240601f601
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2026-01-24 11:25:27 +0100

    readded namingStrategy bean to fix missing fields in API responses

--- discorddeputy-api/src/main/java/com/freberg/discorddeputy/ApiApplication.java
+++ discorddeputy-api/src/main/java/com/freberg/discorddeputy/ApiApplication.java
@@ -4,14 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.core.dialect.JdbcPostgresDialect;
+import org.springframework.data.relational.core.mapping.NamingStrategy;
+import org.springframework.data.relational.core.mapping.RelationalPersistentProperty;
 
 @SpringBootApplication
 @SuppressWarnings("unused")
 public class ApiApplication {
 
     @Bean
-	  JdbcPostgresDialect dialect() {
-		  return JdbcPostgresDialect.INSTANCE;
+    JdbcPostgresDialect dialect() {
+        return JdbcPostgresDialect.INSTANCE;
+    }
+
+    @Bean
+    NamingStrategy namingStrategy() {
+        return new NamingStrategy() {
+            @Override
+            public String getColumnName(RelationalPersistentProperty property) {
+                return property.getName();
+            }
+        };
     }
 
     public static void main(String[] args) {
