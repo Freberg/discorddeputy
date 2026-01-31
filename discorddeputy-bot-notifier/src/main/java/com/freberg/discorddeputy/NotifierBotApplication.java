cac526ef96810e34a66a2b05e00cafa789f15490
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2026-01-31 11:03:01 +0100

    removed jackson3 kotlin workaround for spring-cloud

--- discorddeputy-bot-notifier/src/main/java/com/freberg/discorddeputy/NotifierBotApplication.java
+++ discorddeputy-bot-notifier/src/main/java/com/freberg/discorddeputy/NotifierBotApplication.java
@@ -5,14 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.function.json.JacksonMapper;
-import org.springframework.cloud.function.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
-import tools.jackson.core.StreamReadFeature;
-import tools.jackson.databind.DeserializationFeature;
-import tools.jackson.databind.SerializationFeature;
-import tools.jackson.datatype.joda.JodaModule;
-import tools.jackson.module.kotlin.KotlinModule;
 
 import java.util.function.Consumer;
 
@@ -32,23 +25,6 @@ public class NotifierBotApplication {
             log.info("Received notification with ID \"{}\" from \"{}\"", notification.getId(), notification.getTitle());
             discordNotifier.onNewNotification(notification);
         };
-    }
-
-    /*
-    Workaround for bug in spring-cloud-function where the wrong kotlin module is being used, can be removed once fixed
-    https://github.com/spring-cloud/spring-cloud-function/issues/1319
-     */
-    @Bean
-    public JsonMapper jsonMapper() {
-        var jackson3Mapper = tools.jackson.databind.json.JsonMapper.builder()
-                .addModule(new KotlinModule.Builder().build())
-                .addModule(new JodaModule())
-                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
-                .configure(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION, false)
-                .build();
-        return new JacksonMapper(jackson3Mapper);
     }
 
     public static void main(String[] args) {
